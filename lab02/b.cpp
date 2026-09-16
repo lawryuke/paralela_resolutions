3c5345de2d34ad6d1dbe915066b8291e7a6cc636
kimyork32 <CaronteGG.20851@gmail.com>
2026-09-16 03:28:42 -0500

    implement cache blocking

--- lab02/b.cpp
+++ lab02/b.cpp
@@ -60,6 +60,36 @@ void improvement_mult_matrix(double**& A, double**& B, double**& C, int n) {
     }
 }
 
+/**
+ * @brief multiply matrix A by B and store the result in C using cache blocking
+ * @details using block multiplication approach to optimize cache capacity usage
+ * C = AxB
+ * @param A left matrix A
+ * @param B right matrix B
+ * @param C resulting matrix C
+ * @param n size matrix nxn
+ * @param b size of the block
+*/
+void blocked_mult_matrix(double**& A, double**& B, double**& C, int n, int b = 64) {
+    for (int ii = 0; ii < n; ii += b) {
+        for (int kk = 0; kk < n; kk += b) {
+            for (int jj = 0; jj < n; jj += b) {
+                int iMax = std::min(ii + b, n);
+                int kMax = std::min(kk + b, n);
+                int jMax = std::min(jj + b, n);
+                for (int i = ii; i < iMax; ++i) {
+                    for (int k = kk; k < kMax; ++k) {
+                        double aik = A[i][k];
+                        for (int j = jj; j < jMax; ++j) {
+                            C[i][j] += aik * B[k][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 void experiment() {
     double** A = nullptr;
     double** B = nullptr;
@@ -94,6 +124,19 @@ void experiment() {
         time = delta / 1000.0;
 
         std::cout << "performance i-k-j multiply time: " << time << " ms" << std::endl;
+
+        assign_matrix(C, n, 0.0);
+
+        start = std::chrono::steady_clock::now();
+
+        // multiply AxB with block cache approach
+        blocked_mult_matrix(A, B, C, n, 64); // blocks of 64x64
+
+        end = std::chrono::steady_clock::now();
+        delta = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+        time = delta / 1000.0;
+
+        std::cout << "blocked multiply time: " << time << " ms" << std::endl;
 
         // destructor
         for (int i = 0; i < n; i++) {
